7d40cfd3abaf8c4794aa9d164e3a17cecb0eb11b
zeki can eksi <zekicaneksi@gmail.com>
2023-03-08 18:57:32 +0300

    playing with mui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
-import styles from '@/styles/Home.module.css'
+import styles from "@/styles/Home.module.css";
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
 
 export default function Home() {
-  return(<p>hello</p>)
+  return (
+    <Box
+      sx={{
+        width: 300,
+        height: 300,
+        backgroundColor: "primary.dark",
+        "&:hover": {
+          backgroundColor: "primary.main",
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    >
+      <Typography>Hello</Typography>
+    </Box>
+  );
 }
