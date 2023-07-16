5147c2076dc33a35f803c7951e17e607e4b644c3
zeki can eksi <zekicaneksi@gmail.com>
2023-07-16 16:58:09 +0300

    todoitem content textarea is max height now

--- frontend/src/components/home/TodoBox/Dialogs/ContentDialog.tsx
+++ frontend/src/components/home/TodoBox/Dialogs/ContentDialog.tsx
@@ -1,5 +1,12 @@
 import { todoItem } from "@/pages/home/[[...fieldPath]]";
-import { Dialog, Card, CardContent, TextField, Button, CircularProgress } from "@mui/material";
+import {
+  Dialog,
+  Card,
+  CardContent,
+  TextField,
+  Button,
+  CircularProgress,
+} from "@mui/material";
 import { useState } from "react";
 
 export default function ContentDialog(props: {
@@ -8,35 +15,56 @@ export default function ContentDialog(props: {
 }) {
   const { todoItem, onClose } = props;
 
-  const [textFieldValue, setTextFieldValue] = useState<string | undefined>(todoItem.content)
-    const [loading, setLoading] = useState<boolean>(false)
+  const [textFieldValue, setTextFieldValue] = useState<string | undefined>(
+    todoItem.content
+  );
+  const [loading, setLoading] = useState<boolean>(false);
 
-    function handleOKBtn(){
-        setLoading(true)
-        onClose(true,textFieldValue)
-    }
+  function handleOKBtn() {
+    setLoading(true);
+    onClose(true, textFieldValue);
+  }
 
   return (
-    <Dialog open={true} onClose={() => {onClose(false)}} fullWidth={true}>
-      <Card>
+    <Dialog
+      open={true}
+      onClose={() => {
+        onClose(false);
+      }}
+      fullWidth={true}
+      PaperProps={{ sx: { height: "100%" } }}
+    >
+      <Card sx={{ height: "100%" }}>
         <CardContent
           sx={{
             display: "flex",
             flexDirection: "column",
-            justifyContent: "center",
+            justifyContent: "space-between",
             gap: "20px",
+            height: "100%",
+            boxSizing: "border-box",
           }}
         >
           <TextField
             label="Content"
             multiline
-            rows={4}
             value={textFieldValue}
-            onChange={(event) => {setTextFieldValue(event.target.value)}}
+            onChange={(event) => {
+              setTextFieldValue(event.target.value);
+            }}
             fullWidth={true}
+            sx={{
+              height: "100%",
+              "& .MuiInputBase-root": {
+                height: "100%",
+              },
+              "& textarea": { height: "100% !important" },
+            }}
           />
-          {loading && <CircularProgress sx={{alignSelf: 'center'}}/>}
-          <Button variant="contained" onClick={handleOKBtn}>OK</Button>
+          {loading && <CircularProgress sx={{ alignSelf: "center" }} />}
+          <Button variant="contained" onClick={handleOKBtn}>
+            OK
+          </Button>
         </CardContent>
       </Card>
     </Dialog>
